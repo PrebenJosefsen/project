5cc5d8fe3bad4c93e3679b57fe3094f90d99ce20
Preben Josefsen <preben@josefsen.net>
2020-04-14 16:06:04 +0200

    recipe list working

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model'
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A test recipe 1', 'This is a simple test recipe for demo', 'https://www.w3schools.com/images/w3schools_green.jpg'),
+    new Recipe('A test recipe 2', 'This is a simple test recipe for demo', 'https://www.w3schools.com/images/w3schools_green.jpg'),
+  ];
 
   constructor() { }
 
